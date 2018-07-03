7aa050212646178a17a55908c74482d13c5f4e20
bert_cai <bert_cai@163.com>
2018-07-03 21:14:14 +0800

    init mall

---
+++ src/view/test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="test">
+    this is id:{{$route.params.testId}}
+    <br />
+    this is name:{{$route.params.testName}}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'hello vue'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
