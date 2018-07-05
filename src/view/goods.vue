3d7c4779515c6374c9482ed84976f364cee01e4b
bert_cai <bert_cai@163.com>
2018-07-05 21:27:05 +0800

    add coding vue-router

---
+++ src/view/goods.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="goods">
+    This is goods
+    <span>{{$route.query.goodsId}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'goods'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/view/test.vue
+++ src/view/test.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="test">
+    <!--动态路由-->
     this is id:{{$route.params.testId}}
     <br />
     this is name:{{$route.params.testName}}
+
+    <!--嵌套路由-->
     <router-link to="/test/title1">标题1</router-link>
     <router-link to="/test/title2">标题2</router-link>
     <router-view></router-view>
+
+    <!--编程式路由-->
+    <div @click="gotoGoods">跳转到商品详情页面</div>
   </div>
 </template>
 
@@ -14,6 +20,19 @@ export default {
   data () {
     return {
       msg: 'hello vue'
+    }
+  },
+  methods: {
+    gotoGoods () {
+      this.$router.push({
+        path: '/goods?goodsId=666'
+      })
+      // this.$route.push({
+      //   path: 'goods',
+      //   query: {
+      //     goodsId: 666
+      //   }
+      // })
     }
   }
 }
